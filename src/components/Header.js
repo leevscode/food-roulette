c9c5b1a4a09ff5b66cc5bffae6067924fc8d6894
vitaZ-dev <vitaminzdev@gmail.com>
2023-06-27 11:57:02 +0900

    feat: 헤더 영역 1차 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="p-7 bg-black">
-      <div className="flex flex-wrap align-items-center justify-between">
-        <ul className="flex items-center justify-center gap-4">
-          <li>
+    <>
+      <header className="p-7 bg-black">
+        <div className="flex flex-wrap align-items-center justify-between">
+          <div>
             <Link to="/" className="text-white hover:text-orange-600">
-              타이틀화면
+              로그아웃
             </Link>
-          </li>
-          <li>
-            <Link to="/main" className="text-white hover:text-orange-600">
-              메인화면=룰렛
+          </div>
+          <ul className="flex items-center justify-center gap-4">
+            <li>
+              <Link to="/main" className="text-white hover:text-green-600">
+                룰렛
+              </Link>
+            </li>
+            <li>
+              <Link to="/menu" className="text-white hover:text-emerald-600">
+                메뉴 설정
+              </Link>
+            </li>
+            <li>
+              <Link to="/calender" className="text-white hover:text-green-600">
+                달력
+              </Link>
+            </li>
+            <li>
+              <Link to="/history" className="text-white hover:text-emerald-400">
+                히스토리
+              </Link>
+            </li>
+          </ul>
+          <div>
+            <Link to="/" className="text-white hover:text-orange-600">
+              한도
             </Link>
-          </li>
-          <li>
-            <Link to="/calender" className="text-white hover:text-orange-600">
-              calender
-            </Link>
-          </li>
-          <li>
-            <Link to="/menu" className="text-white hover:text-yellow-600">
-              메뉴설정
-            </Link>
-          </li>
-          <li>
-            <Link to="/history" className="text-white hover:text-amber-400">
-              History
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </header>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
