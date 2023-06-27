be68a47e13ede8e7b7cce299b34db8b91f70c3de
vitaZ-dev <vitaminzdev@gmail.com>
2023-06-27 15:23:34 +0900

    feat: Main 영역 기능 추가
    - 한도 입력받는 영역 작업 시작

--- src/components/LimitSetting.js
+++ src/components/LimitSetting.js
@@ -1,7 +1,44 @@
 import React from "react";
 
-const LimitSetting = () => {
-  return <div>LimitSetting</div>;
+const LimitSetting = ({ setIsLimit }) => {
+  const bgStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "pink",
+  };
+  const inputContainer = {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  const inputBox = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const handleInputLimit = () => {
+    // db에 한도 설정 값 저장해야 함
+    setIsLimit(10000);
+  };
+
+  return (
+    <div style={bgStyle}>
+      <div style={inputContainer}>
+        <div style={inputBox}>
+          <p>한도를 입력해주세요</p>
+          <input type="text" />
+          <button onClick={handleInputLimit}>입력</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LimitSetting;
