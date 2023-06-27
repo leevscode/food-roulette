f70b813841b32f5909c381512f2b1f3b34c82d98
vitaZ-dev <vitaminzdev@gmail.com>
2023-06-27 12:46:18 +0900

    feat: Menu 영역 base templete 생성 완료

--- src/components/ShowMenuList.js
+++ src/components/ShowMenuList.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 const ShowMenuList = () => {
+  const [showMenu, setShowMenu] = useState([]);
+
   return (
     <div>
-      <>메뉴리스트</>
+      <h2>메뉴리스트</h2>
+      <div>
+        <p>(db에서 불러온 메뉴리스트들)</p>
+        <div
+          style={{
+            border: "1px solid red",
+            padding: 8,
+            background: "pink",
+          }}
+        >
+          <p>메뉴이름</p>
+          <p>해시태그들</p>
+          <button>해시태그수정</button>
+          <button>메뉴삭제</button>
+        </div>
+      </div>
     </div>
   );
 };
