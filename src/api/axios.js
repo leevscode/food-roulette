8215f441de2b5b07014cae8d7b7a4e13a0ca8b6a
vitaZ-dev <vitaminzdev@gmail.com>
2023-06-29 16:46:43 +0900

    test: 백엔드 쪽 통신테스트 중

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5003",
+  baseURL: "http://http://192.168.0.144:5003",
+  // baseURL: "http://localhost:5003",
   timeout: 1000,
   headers: {
     "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Loading from "../components/Loading";
 import MenuInput from "../components/MenuInput";
 import ShowMenuList from "../components/ShowMenuList";
+import { axiosInstance } from "../api/axios";
+import axios from "axios";
 
 const Menu = () => {
   // 로딩 처리
@@ -13,6 +15,13 @@ const Menu = () => {
   useEffect(() => {
     setIsLoading(false);
   }, []);
+  const testAxios = () => {
+    axios
+      .get("http://192.168.0.144:5003/menus/1/menus")
+      .then(res => res.data)
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  };
 
   return (
     <div>
@@ -25,6 +34,7 @@ const Menu = () => {
       <hr />
       {toggle && <MenuInput />}
       {toggle || <ShowMenuList />}
+      <button onClick={testAxios}>통신 테스트</button>
     </div>
   );
 };
