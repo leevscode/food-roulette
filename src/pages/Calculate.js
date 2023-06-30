e51134eb1fd07f6443a3f974bcb8ea68213d6437
leevscode <leevscode@gmail.com>
2023-06-30 17:50:08 +0900

    feat:  기능 추가 설명 업데이트

--- src/pages/Calculate.js
+++ src/pages/Calculate.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Limit, ProgressContainer, ProgressFill, ProgressFillInner, ProgressText } from "../style/CalculateCSS";
+import {
+  Limit,
+  ProgressContainer,
+  ProgressFill,
+  ProgressFillInner,
+  ProgressText,
+} from "../style/CalculateCSS";
 import ShowMonth from "../components/ShowMonth";
 
-
-// useState 훅을 사용하여 remainingBalance 상태를 관리하고, 
-// 초기값으로 500000을 설정. 
+// useState 훅을 사용하여 remainingBalance 상태를 관리하고,
+// 초기값으로 500000을 설정.
 // progressPercent 상태도 useState 훅을 이용해 관리
 // 계산 로직은 별도의 함수인 calculateProgress 내부에 구현됨
-// useEffect 훅을 사용하여 remainingBalance 또는 
+// useEffect 훅을 사용하여 remainingBalance 또는
 // totalBalance 값이 변경될 때마다 이 함수를 호출
 // 이를 통해 진행 상태의 백분율이 적절하게 업데이트되도록 한다
+// 추가 코드 진행 완료함.
 const Calculate = () => {
   const [remainingBalance, setRemainingBalance] = useState(400000);
   const totalBalance = 500000;
@@ -34,7 +40,13 @@ const Calculate = () => {
           </ProgressFill>
         </ProgressContainer>
       </Limit>
-      <div style={{ textAlign: "center", display: "flex", justifyContent: "center" }}>
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <ShowMonth />
       </div>
     </>
