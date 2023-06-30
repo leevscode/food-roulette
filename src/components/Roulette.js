c90c32b005ac5cead25a31108d3d00818f45006d
vitaZ-dev <vitaminzdev@gmail.com>
2023-06-30 09:43:13 +0900

    feat: CORS 문제 해결을 위한 Proxy 설정

--- src/components/Roulette.js
+++ src/components/Roulette.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Wheel } from "react-custom-roulette";
 import { RouletteBox } from "../style/MainCSS";
+import Modal from "antd/es/modal/Modal";
 
 const Roulette = () => {
   // 룰렛데이터
   const [startSpin, setStartSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [isSpinning, setIsSpinning] = useState(false);
-  // const [spinResult, setSpinResult] = useState("");
+  const [spinResult, setSpinResult] = useState("");
 
   const data = [
     { id: 1, option: 10 },
@@ -30,56 +31,79 @@ const Roulette = () => {
     setStartSpin(true);
     setIsSpinning(true);
     // state는 비동기로 저장되어 실시간 반영이 안됨 - 추후 수정 필요
-    // setSpinResult(data[prizeNumber].option);
+    setSpinResult(data[prizeNumber].option);
+    console.log(spinResult);
+  };
+
+  // 모달 관련 state
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
   };
 
   // JSX
   return (
-    <RouletteBox>
-      <Wheel
-        mustStartSpinning={startSpin}
-        prizeNumber={prizeNumber}
-        data={data}
-        outerBorderColor={["#eee"]}
-        outerBorderWidth={[5]}
-        innerBorderColor={["#000"]}
-        innerBorderWidth={[10]}
-        radiusLineColor={["#dedede"]}
-        radiusLineWidth={[2]}
-        textColors={["#ffffff"]}
-        fontSize={[30]}
-        perpendicularText={[true]}
-        backgroundColors={[
-          "rgb(246,110,100)",
-          "rgb(250,169,78)",
-          "rgb(251,224,93)",
-          "rgb(138,224,93)",
-          "rgb(80,220,240)",
-          "rgb(155,180,238)",
-          "rgb(180,140,238)",
-          "rgb(255,174,200)",
-        ]}
-        spinDuration={[0.5]}
-        onStopSpinning={() => {
-          setStartSpin(false);
-          setIsSpinning(false);
-        }}
-        pointerProps={{
-          src: "/images/pointer.png",
-          style: {
-            transform: "rotate(45.75deg) translateX(-6px) translateY(24px)",
-          },
-        }}
-      />
+    <>
+      <RouletteBox>
+        <Wheel
+          mustStartSpinning={startSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          outerBorderColor={["#eee"]}
+          outerBorderWidth={[5]}
+          innerBorderColor={["#000"]}
+          innerBorderWidth={[10]}
+          radiusLineColor={["#dedede"]}
+          radiusLineWidth={[2]}
+          textColors={["#ffffff"]}
+          fontSize={[30]}
+          perpendicularText={[true]}
+          backgroundColors={[
+            "rgb(246,110,100)",
+            "rgb(250,169,78)",
+            "rgb(251,224,93)",
+            "rgb(138,224,93)",
+            "rgb(80,220,240)",
+            "rgb(155,180,238)",
+            "rgb(180,140,238)",
+            "rgb(255,174,200)",
+          ]}
+          spinDuration={[0.5]}
+          onStopSpinning={() => {
+            setStartSpin(false);
+            setIsSpinning(false);
+          }}
+          pointerProps={{
+            src: "/images/pointer.png",
+            style: {
+              transform: "rotate(45.75deg) translateX(-6px) translateY(24px)",
+            },
+          }}
+        />
 
-      <button onClick={handleSpinRoulette} disabled={isSpinning}>
-        SPIN
-      </button>
-      <br />
-      {!startSpin
-        ? `오늘의 결과는? ${data[prizeNumber].option}`
-        : "룰렛이 돌아가고 있습니다..."}
-    </RouletteBox>
+        <button onClick={handleSpinRoulette} disabled={isSpinning}>
+          SPIN
+        </button>
+        <br />
+        {!startSpin
+          ? `오늘의 결과는? ${data[prizeNumber].option}`
+          : "룰렛이 돌아가고 있습니다..."}
+      </RouletteBox>
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered
+      >
+        <p>{spinResult}</p>
+      </Modal>
+    </>
   );
 };
 
