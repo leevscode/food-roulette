475563ff522a80f1b7c932d20c7e05bff970658d
leevscode <leevscode@gmail.com>
2023-09-18 17:16:13 +0900

    README 추가 작업

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,8 @@ const About = () => {
     <Aboutbg>
       <Aboutbox>
         고생하신분들 <br />
-        BE : 이민용 임정호 신여운 <br />
-        FE : 이동은 황지현
+        BE : <a href="https://github.com/JeongHoBBang/food_roulette">이민용 임정호 신여운</a> <br />
+        FE : <a href="https://github.com/leevscode">이동은</a> <a href="https://github.com/vitaZ-dev">황지현</a>
       </Aboutbox>
     </Aboutbg>
   );
