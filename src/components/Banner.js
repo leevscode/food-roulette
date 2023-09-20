8efcfd7718fd46e849911d36f0d42f377c9fef29
leevscode <leevscode@gmail.com>
2023-09-20 18:29:17 +0900

    코드 수정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,12 +9,12 @@ const banner = () => {
         width={200}
         height={200}
       /> */}
-      <img
+      {/* <img
         src="/images/green.png"
         alt="그린컴퓨터아트학원"
         width={270}
         height={270}
-      />
+      /> */}
     </div>
   );
 };
