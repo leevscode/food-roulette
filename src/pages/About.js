9f888ebb08a90486d34db252609103ddb167ddb4
leevscode <leevscode@gmail.com>
2023-09-20 18:50:43 +0900

    진짜 최종 코드 수정

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,21 @@ const About = () => {
     <Aboutbg>
       <Aboutbox>
         고생하신분들 <br />
-        BE : <a href="https://github.com/JeongHoBBang/food_roulette">이민용 임정호 신여운</a> <br />
-        FE : <a href="https://github.com/leevscode">이동은</a> <a href="https://github.com/vitaZ-dev">황지현</a>
+        <span>BE</span>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/JeongHoBBang/food_roulette"
+        >
+          이민용 임정호 신여운
+        </a>{" "}
+        FE{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/leevscode">
+          이동은
+        </a>{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/vitaZ-dev">
+          황지현
+        </a>
       </Aboutbox>
     </Aboutbg>
   );
